c730332879ef270eba7897936c774fda82daa528
Victor Meunier <victormeunier.dev@gmail.com>
2018-11-17 16:38:55 +0200

    Fixes issues #7
    Removed the bitwise not to match the data representation of the training data of the SVM. Tweaked the gamma of the SVM value to get better predictions.

--- digits.py
+++ digits.py
@@ -1,25 +1,27 @@
 from sklearn import datasets
 from sklearn import svm
 import matplotlib.pyplot as plt
+import numpy as np
 
+'''
 digits = datasets.load_digits()
 
-clf = svm.SVC(gamma=0.01, C=100)
+clf = svm.SVC(gamma=0.001, C=100)
 
 x,y = digits.data[:-1], digits.target[:-1]
 clf.fit(x,y)
 
 print(digits.data[[-5]])
-print(digits.data[[-5]].shape)
 print("Prediction ", clf.predict(digits.data[[-11]]))
 
 plt.imshow(digits.images[-11], cmap=plt.cm.gray_r, interpolation="nearest")
 plt.show()
+'''
 
 class SVM():
     def __init__(self):
         self.digits = datasets.load_digits()
-        self.clf = svm.SVC(gamma=0.01, C=100)
+        self.clf = svm.SVC(gamma=0.0001, C=100)
     
     def train(self):
         x,y = self.digits.data[:-1], self.digits.target[:-1]
